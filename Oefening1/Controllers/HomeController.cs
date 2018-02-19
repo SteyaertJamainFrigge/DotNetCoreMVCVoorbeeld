962900f1e289771edca2d477eeb6aea60d0a1f7e
SteyaertJamainFrigge <frigge.steyaert.jamain@student.howest.be>
2018-02-19 11:46:22 +0100

    groentenlijst gemaakt + laten toen

--- Oefening1/Controllers/HomeController.cs
+++ Oefening1/Controllers/HomeController.cs
@@ -8,9 +8,20 @@ namespace Oefening1.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index(string name)
+
+        private string[] groenten = { "Rode kool", "Spruiten", "Worte", "Brocoli", "Spinazie", "Bloemkool", "Chinese Kool", "" };
+
+        public ViewResult Index(string name)
         {
-            return $"Hello, it's me {name}";
+            ViewBag.Groet = DateTime.Now.Hour < 12 ? "Goeimorgen" : "GoeieAvond";
+            return View();
+        }
+
+        public ViewResult Groenten(string name)
+        {
+            ViewBag.Groenten = groenten;
+
+            return View();
         }
     }
 }
